bf7bfef7f34020e01786a2a482629fe88b2feb78
Marquis Ogre <marquisogre@gmail.com>
2025-12-06 17:27:37 +0530

    Andriod Build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.51da082f6d91482081f3cff4e5f6f800',
+  appId: 'com.artisanpparels.artisan',
   appName: 'artisan-invoicess',
   webDir: 'dist'
 };
